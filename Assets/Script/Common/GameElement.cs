ceebe2b5cab6732c19717f51ef3ae201b9b732c2
melonmilk114 <melonmilk114@gmail.com>
2025-07-30 22:24:22 +0900

    게임 오브젝트의 기본 클래스 추가

---
+++ Assets/Script/Common/GameElement.cs
@@ -0,0 +1,221 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngineInternal;
+
+namespace Melon
+{
+    public class GameElement : MonoBehaviour
+    {
+        public bool isInit = false;
+        
+        public void Awake()
+        {
+            OnAwakeFunc();
+        }
+
+        /// <summary>
+        /// Awake시 호출됨
+        /// </summary>
+        public virtual void OnAwakeFunc()
+        {
+        }
+        
+        public void Start()
+        {
+            OnStartFunc();
+        }
+
+        public virtual void OnStartFunc()
+        {
+        }
+        public void OnEnable()
+        {
+            OnEnableFunc();
+        }
+    
+        public void OnDisable()
+        {
+            OnDisableFunc();
+        }
+    
+        public void OnDestroy()
+        {
+            OnDestroyFunc();
+        }
+    
+        public virtual void OnEnableFunc()
+        {
+            
+        }
+        
+        public virtual void OnDisableFunc()
+        {
+            
+        }
+        
+        public virtual void OnDestroyFunc()
+        {
+            
+        }
+
+        public virtual void DoPreShow(object inData = null)
+        {
+            
+        }
+        public virtual void DoPreShow(object inData = null, ActionResult inActionResult = null)
+        {
+            inActionResult?.OnSuccess();
+        }
+        public virtual void DoPostShow(object inData = null)
+        {
+            
+        }
+        public virtual void DoPostShow(object inData = null, ActionResult inActionResult = null)
+        {
+            inActionResult?.OnSuccess();
+        }
+        public virtual void DoShowCheck(object inData = null)
+        {
+            
+        }
+        public virtual void DoShowCheck(object inData = null, ActionResult inActionResult = null)
+        {
+            inActionResult?.OnSuccess();
+        }
+        public void DoShow(object inData = null, ActionResult inActionResult = null)
+        {
+            if (inActionResult == null)
+            {
+                DoShowCheck(inData);
+                DoPreShow(inData);
+                
+                gameObject.SetActive(true);
+                
+                DoPostShow(inData);
+            }
+            else
+            {
+                DoShowCheck(inData, new ActionResult()
+                {
+                    onSuccess = () =>
+                    {
+                        DoPreShow(inData, new ActionResult()
+                        {
+                            onSuccess = () =>
+                            {
+                                gameObject.SetActive(true);
+                            
+                                DoPostShow(inData, new ActionResult()
+                                {
+                                    onSuccess = () =>
+                                    {
+                                        inActionResult?.OnSuccess();
+                                    },
+                                    onFail = (err) =>
+                                    {
+                                        Debug.LogError(err);
+                                        inActionResult?.OnFail(err);
+                                    }
+                                });
+                            },
+                            onFail = (err) =>
+                            {
+                                Debug.LogError(err);
+                                inActionResult?.OnFail(err);
+                            }
+                        });
+                    },
+                    onFail = (err) =>
+                    {
+                        Debug.LogError(err);
+                        inActionResult?.OnFail(err);
+                    }
+                });
+            }
+        }
+        public virtual void DoPreHide(object inData = null)
+        {
+            
+        }
+        public virtual void DoPreHide(object inData = null, ActionResult inActionResult = null)
+        {
+            inActionResult?.OnSuccess();
+        }
+        public virtual void DoPostHide(object inData = null)
+        {
+            
+        }
+        public virtual void DoPostHide(object inData = null, ActionResult inActionResult = null)
+        {
+            inActionResult?.OnSuccess();
+        }
+        public virtual void DoHideCheck(object inData = null)
+        {
+            
+        }
+        public virtual void DoHideCheck(object inData = null, ActionResult inActionResult = null)
+        {
+            inActionResult?.OnSuccess();
+        }
+    
+        public virtual void DoHide(object inData = null, ActionResult inActionResult = null)
+        {
+            if (inActionResult == null)
+            {
+                DoHideCheck(inData);
+                DoPreHide(inData);
+                
+                gameObject.SetActive(false);
+                
+                DoPostHide(inData);
+            }
+            else
+            {
+                DoHideCheck(inData, new ActionResult()
+                {
+                    onSuccess = () =>
+                    {
+                        DoPreHide(inData, new ActionResult()
+                        {
+                            onSuccess = () =>
+                            {
+                                gameObject.SetActive(false);
+                            
+                                DoPostHide(inData, new ActionResult()
+                                {
+                                    onSuccess = () =>
+                                    {
+                                        inActionResult?.OnSuccess();
+                                    },
+                                    onFail = (err) =>
+                                    {
+                                        Debug.LogError(err);
+                                        inActionResult?.OnFail(err);
+                                    }
+                                });
+                            },
+                            onFail = (err) =>
+                            {
+                                Debug.LogError(err);
+                                inActionResult?.OnFail(err);
+                            }
+                        });
+                    },
+                    onFail = (err) =>
+                    {
+                        Debug.LogError(err);
+                        inActionResult?.OnFail(err);
+                    }
+                });
+            }
+        }
+
+        public virtual void Init()
+        {
+            isInit = true;
+        }
+    }
+}
+
