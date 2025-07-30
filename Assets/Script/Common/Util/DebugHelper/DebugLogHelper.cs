37f48252af296b2f3cd3ea4790c8e7bdd855c339
melonmilk114 <melonmilk114@gmail.com>
2025-07-30 22:17:10 +0900

    DebugLoghelper 추가
    유틸성으로 추가

---
+++ Assets/Script/Common/Util/DebugHelper/DebugLogHelper.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public static class DebugLogHelper
+{
+    public static void Log(string message, object caller = null, string methodName = "")
+    {
+        string callerName = caller?.GetType().Name ?? "UnknownCaller";
+        string methodPart = string.IsNullOrEmpty(methodName) ? "" : $" -> {methodName}";
+
+        Debug.Log($"{message}\n\ncallstack\n\n{callerName}{methodPart}");
+    }
+
+    public static void LogWarning(string message, object caller = null, string methodName = "")
+    {
+        string callerName = caller?.GetType().Name ?? "UnknownCaller";
+        string methodPart = string.IsNullOrEmpty(methodName) ? "" : $" -> {methodName}";
+
+        Debug.LogWarning($"{message}\n\ncallstack\n\n{callerName}{methodPart}");
+    }
+
+    public static void LogError(string message, object caller = null, string methodName = "")
+    {
+        string callerName = caller?.GetType().Name ?? "UnknownCaller";
+        string methodPart = string.IsNullOrEmpty(methodName) ? "" : $" -> {methodName}";
+
+        Debug.LogError($"{message}\n\ncallstack\n\n{callerName}{methodPart}");
+    }
+}
